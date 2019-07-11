c1a2a39d4111da8cb1ec9e1678954886beea51ca
Mateo <mateo.webdev@gmail.com>
2019-07-11 10:14:27 +0200

    Change class component to Stateless comoponent

--- bill-generator/src/App.js
+++ bill-generator/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Info from './components/Info'
+import EstimateForm from './components/EstimateForm'
 
 class App extends Component {
   state ={
@@ -9,12 +10,22 @@ class App extends Component {
 
 handleShowEstimateForm = evt => {
   console.log(evt);
+  this.setState({
+    showForm: !this.state.showForm
+  })
 };
 
   render(){
      return (
+       //React-Fragment
+       <div>
       <Info onHandleShowEstimateForm={this.handleShowEstimateForm}/>
+      <br/>
+      {this.state.showForm && <EstimateForm/>}
+      </div>
     );
+    //this.state.showForm && <EstimateForm/> 
+    //Si this.state.showForm est TRUE alors on affiche <EstimateForm/> sinon on ne l'affiche jamais
   }
 }
 
--- bill-generator/src/components/Info.jsx
+++ bill-generator/src/components/Info.jsx
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-
-class Info extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
+import React from 'react';
+ 
+const Info = (props) => {
+  return (  
+    <div>
     <h2>Bienvenu sur votre générateur de devis</h2>
     <div>
-      Créer un devis ? C'est simple : Cliquez sur <button onClick={this.props.onHandleShowEstimateForm}> nouveau devis</button>
+      Créer un devis ? C'est simple : Cliquez sur 
+      <button onClick={props.onHandleShowEstimateForm}> nouveau devis</button>
     </div>
     </div>
-         );
-    }
+  );
 }
  
 export default Info;
+
